65500f919e691fbb369c0275bcb05dd52950fda3
Felipe Apostol <flipjs.io@gmail.com>
2016-01-17 21:20:00 +0000

    add sample code for attaching event handler on column

--- main.js
+++ main.js
@@ -5,27 +5,43 @@ import data from './data'
 import './css/react-bootstrap-table-all.css'
 import './css/custom.css'
 
-function onRowSelect (row, isSelected) {
-  console.log(row)
-  console.log('selected: ' + isSelected)
+// function onRowSelect (row, isSelected) {
+//   console.log(row)
+//   console.log('selected: ' + isSelected)
+// }
+
+const onClick = (cell, row) => {
+  console.log('cell:', cell)
+  console.log('row:', row)
 }
 
-var selectRowProp = {
-  mode: 'radio',
-  clickToSelect: true,
-  bgColor: 'rgb(238, 193, 213)',
-  onSelect: onRowSelect,
-  hideSelectColumn: true
+const cursorClass = {
+  cursor: 'pointer'
 }
+
+const dataFormat = (cell, row) => {
+  return (
+    <div style={cursorClass} onClick={() => onClick(cell, row)}>{cell}</div>
+  )
+}
+
+// var selectRowProp = {
+//   mode: 'radio',
+//   clickToSelect: true,
+//   bgColor: 'rgb(238, 193, 213)',
+//   onSelect: onRowSelect,
+//   hideSelectColumn: true
+// }
 
 ReactDOM.render(
-  <div className='container'>
+  <div className='container-fluid'>
     <h3>Table [{' ' + data.length + ' '}]</h3>
-    <BootstrapTable data={data} search striped hover pagination selectRow={selectRowProp}>
+    <BootstrapTable data={data} search striped hover pagination>
       <TableHeaderColumn
         dataField='id'
         isKey
         dataAlign='right'
+        width='50'
         dataSort>
         ID
       </TableHeaderColumn>
@@ -35,7 +51,7 @@ ReactDOM.render(
       <TableHeaderColumn dataField='subDomain' dataSort>
         Sub-Domain
       </TableHeaderColumn>
-      <TableHeaderColumn dataField='ip' dataSort>
+      <TableHeaderColumn dataField='ip' dataFormat={dataFormat} dataSort>
         IP Address
       </TableHeaderColumn>
     </BootstrapTable>
